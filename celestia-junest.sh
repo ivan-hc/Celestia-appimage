75d8341764aac3a93987381b36bebdab38ac0b44
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-04-28 00:24:22 +0200

    Update celestia-junest.sh
    - fix libtheora
    - add gtkglext

--- celestia-junest.sh
+++ celestia-junest.sh
@@ -2,7 +2,7 @@
 
 APP=celestia
 BIN="$APP" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
-DEPENDENCES="" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
+DEPENDENCES="gtkglext" #SYNTAX: "APP1 APP2 APP3 APP4...", LEAVE BLANK IF NO OTHER DEPENDENCIES ARE NEEDED
 BASICSTUFF="binutils debugedit gzip"
 COMPILERS="base-devel"
 
@@ -14,7 +14,7 @@ BINSAVED="SAVEBINSPLEASE"
 SHARESAVED="SAVESHAREPLEASE"
 #lib_audio_keywords="alsa jack pipewire pulse"
 lib_browser_launcher="gio-launch-desktop libasound.so libatk-bridge libatspi libcloudproviders libdb- libdl.so libedit libepoxy libgtk-3.so.0 libjson-glib libnssutil libpthread.so librt.so libtinysparql libwayland-cursor libX11-xcb.so libxapp-gtk3-module.so libXcursor libXdamage libXi.so libxkbfile.so libXrandr p11 pk"
-LIBSAVED="libSM.so libICE.so libuuid.so pixmap libGLX libsensors.so libpciaccess.so $lib_audio_keywords $lib_browser_launcher"
+LIBSAVED="libSM.so libICE.so libuuid.so pixmap libGLX libsensors.so libpciaccess.so libtheora $lib_audio_keywords $lib_browser_launcher"
 
 [ -n "$lib_browser_launcher" ] && DEPENDENCES="$DEPENDENCES xapp hicolor-icon-theme"
 
@@ -567,6 +567,9 @@ if ! test -d ./Celestia-appimage; then
 	cd -
 	rm -Rf ./Celestia-appimage
 fi
+
+# Fix libtheora.so.0
+cp -r ./"$APP".AppDir/.junest/usr/lib/libtheora.so ./"$APP".AppDir/.junest/usr/lib/libtheora.so.0
 
 #############################################################################
 #	CREATE THE APPIMAGE
